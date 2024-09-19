832905d89ef4e03fa0f9bc6f62011a488449087f
Oksana Boliziuk <oksanabolizyk23@gmail.com>
2024-09-19 16:39:33 +0300

    Updated settings, models and added db.sqlite3 to .gitignore

--- dishops/apps.py
+++ dishops/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class DishopsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'dishops'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "dishops"
