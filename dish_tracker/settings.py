9cce4eb9a33df80631298df652f8e457c3710afd
Oksana Boliziuk <oksanabolizyk23@gmail.com>
2024-10-21 11:19:57 +0300

    Update settings to use environment variables for SECRET_KEY and DEBUG

--- dish_tracker/settings.py
+++ dish_tracker/settings.py
@@ -26,9 +26,10 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
+print("DEBUG:", DEBUG)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
