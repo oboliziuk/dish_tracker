beb2e35232448be9cac694180db7357e825f2ce2
Oksana Boliziuk <oksanabolizyk23@gmail.com>
2024-10-29 11:07:01 +0200

    Update settings for dynamic ALLOWED_HOSTS configuration

--- dish_tracker/settings.py
+++ dish_tracker/settings.py
@@ -31,7 +31,6 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 # https://docs.djangoproject.com/en/3.0/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
