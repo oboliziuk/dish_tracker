58f9cc8fab26470f3e9a6658f4b7327bc54aac2f
Oksana Boliziuk <oksanabolizyk23@gmail.com>
2024-10-29 10:33:47 +0200

    Add build.sh script for deployment

--- dish_tracker/settings.py
+++ dish_tracker/settings.py
@@ -30,6 +30,12 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+# https://docs.djangoproject.com/en/3.0/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
