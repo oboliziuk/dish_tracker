dc4809ae820159315229b1163cb4d0efcf7783bc
Oksana Boliziuk <oksanabolizyk23@gmail.com>
2024-10-30 13:40:56 +0200

    Resolve merge conflicts in base.py and requirements.txt

--- dish_tracker/settings/base.py
+++ dish_tracker/settings/base.py
@@ -10,11 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
-from pathlib import Path
 from dotenv import load_dotenv
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+load_dotenv(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
@@ -23,13 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
-
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
-print("DEBUG:", DEBUG)
-
-ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
@@ -63,7 +57,6 @@ ROOT_URLCONF = "dish_tracker.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        #"DIRS": [BASE_DIR / "templates"],
         "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
@@ -78,17 +71,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "dish_tracker.wsgi.application"
-
-
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
 
 
 # Password validation
@@ -126,7 +108,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-#STATIC_URL = "static/"
 STATIC_URL = "/static/"
 
 # Default primary key field type
