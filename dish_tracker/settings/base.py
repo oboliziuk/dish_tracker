3265951548aad46c8df32aa187de09f0b758b0d5
Oksana Boliziuk <oksanabolizyk23@gmail.com>
2024-11-04 20:24:40 +0200

    Updated static file settings to ensure correct loading of static assets

--- dish_tracker/settings/base.py
+++ dish_tracker/settings/base.py
@@ -13,9 +13,9 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 load_dotenv(BASE_DIR / '.env')
 
@@ -25,6 +25,8 @@ load_dotenv(BASE_DIR / '.env')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
+
+# https://docs.djangoproject.com/en/3.0/ref/settings/#allowed-hosts
 
 
 # Application definition
@@ -44,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -109,6 +112,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- dish_tracker/settings/prod.py
+++ dish_tracker/settings/prod.py
@@ -12,6 +12,9 @@ RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
+#STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
+
+
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
